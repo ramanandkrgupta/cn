9efc55343c1240f771b82ed4d42a17ebe4a9294b
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-08-29 16:03:29 +0530

    Update NewNavbar.jsx

--- components/navigation/NewNavbar.jsx
+++ components/navigation/NewNavbar.jsx
@@ -26,29 +26,29 @@ const NewNavBar = () => {
 
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-gray-900">
-      <div className="relative flex justify-center items-center bg-white rounded-t-lg p-2 w-full">
+      <div className="flex justify-center items-center bg-white rounded-t-lg p-2 w-full h-20">
         <ul className="flex w-full justify-between">
           {menuItems.map((item, index) => (
             <li
               key={index}
-              className={`relative list-none w-16 h-16 z-10 ${
+              className={`relative list-none flex-1 flex justify-center items-center h-full z-10 ${
                 activeIndex === index ? "active" : ""
               }`}
               onClick={() => handleActiveLink(index)}
             >
               <a
                 href="#"
-                className="flex flex-col items-center justify-center h-full text-center"
+                className="flex flex-col items-center justify-center text-center"
               >
                 <span
                   className={`block text-2xl transition-transform duration-500 ${
-                    activeIndex === index ? "transform -translate-y-4" : ""
+                    activeIndex === index ? "transform -translate-y-2" : ""
                   }`}
                 >
                   <IonIcon icon={item.icon} />
                 </span>
                 <span
-                  className={`absolute text-sm font-medium transition-opacity duration-500 ${
+                  className={`text-sm font-medium transition-opacity duration-500 ${
                     activeIndex === index
                       ? "opacity-100 translate-y-2"
                       : "opacity-0"
@@ -60,13 +60,6 @@ const NewNavBar = () => {
             </li>
           ))}
         </ul>
-        <div
-          className="absolute top-0 left-0 w-16 h-16 bg-green-500 rounded-full border-8 border-gray-900 transition-transform duration-500"
-          style={{ transform: `translateX(${activeIndex * 4.5}rem)` }}
-        >
-          <div className="absolute top-1/2 left-[-22px] w-5 h-5 bg-transparent rounded-tr-[20px] shadow-[0px_-10px_0_0_#222327]"></div>
-          <div className="absolute top-1/2 right-[-22px] w-5 h-5 bg-transparent rounded-tl-[20px] shadow-[0px_-10px_0_0_#222327]"></div>
-        </div>
       </div>
     </div>
   );
