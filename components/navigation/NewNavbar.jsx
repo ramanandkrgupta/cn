a63a8c27cf94652bb3fd89728e3e0aed602aacb7
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-08-29 16:38:53 +0530

    Update NewNavbar.jsx

--- components/navigation/NewNavbar.jsx
+++ components/navigation/NewNavbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { IonIcon } from "@ionic/react";
 import {
   homeOutline,
@@ -10,6 +10,7 @@ import {
 } from "ionicons/icons";
 
 const NewNavBar = () => {
+  // Set the first item as active by default
   const [activeIndex, setActiveIndex] = useState(0);
 
   const handleActiveLink = (index) => {
@@ -59,8 +60,11 @@ const NewNavBar = () => {
           ))}
         </ul>
         <div
-          className="absolute top-0 left-0 w-16 h-16 bg-green-500 rounded-full border-8 border-gray-900 transition-transform duration-300 ease-in-out"
-          style={{ left: `calc(${activeIndex} * 20%)`, transform: 'translateX(-50%)' }}
+          className="absolute top-0 left-0 w-16 h-16 bg-green-500 rounded-full border-8 border-gray-900 transition-transform duration-500 ease-in-out"
+          style={{
+            left: `calc(${activeIndex} * 20%)`,
+            transform: "translateX(-50%)",
+          }}
         >
           <div className="absolute top-1/2 left-[-22px] w-5 h-5 bg-transparent rounded-tr-[20px] shadow-[0px_-10px_0_0_#222327]"></div>
           <div className="absolute top-1/2 right-[-22px] w-5 h-5 bg-transparent rounded-tl-[20px] shadow-[0px_-10px_0_0_#222327]"></div>
