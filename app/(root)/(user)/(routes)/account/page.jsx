c465a199320d8263d596702e4f578c5d9a696e26
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-08-30 01:18:57 +0530

    Update page.jsx

--- app/(root)/(user)/(routes)/account/page.jsx
+++ app/(root)/(user)/(routes)/account/page.jsx
@@ -1,10 +1,17 @@
 "use client";
 
+import { Tab } from "@headlessui/react";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import { toast } from 'sonner';
+
+import AddSubject from '@/components/admin/components/AddSubject';
+import ShowData from '@/components/admin/components/ShowData';
+import ChangePassword from '@/components/admin/components/ChangePassword';
+
+const Tabs = ["Account Details", "Playground", "Show Data"];
 
 const AccountPage = () => {
   const router = useRouter();
@@ -43,25 +50,53 @@ const AccountPage = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Account Details</h1>
-      <div className="bg-gray-100 p-4 rounded-lg shadow-md">
-        <p><strong>Name:</strong> {user.name}</p>
-        <p><strong>Email:</strong> {user.email}</p>
-        <p><strong>Phone Number:</strong> {user.phoneNumber}</p>
-        <p><strong>Role:</strong> {user.userRole}</p>
-      </div>
+      <Tab.Group>
+        <Tab.List className="flex space-x-1 sm:w-2/5">
+          {Tabs.map((tab, index) => (
+            <Tab
+              key={index}
+              className={({ selected }) =>
+                `w-full py-2.5 text-base font-semibold text-white outline-none ${
+                  selected && "border-b-4 border-green-400 "
+                }`
+              }
+            >
+              {tab}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels className="mt-3">
+          <Tab.Panel>
+            <div className="bg-gray-100 p-4 rounded-lg shadow-md">
+              <p><strong>Name:</strong> {user.name}</p>
+              <p><strong>Email:</strong> {user.email}</p>
+              <p><strong>Phone Number:</strong> {user.phoneNumber}</p>
+              <p><strong>Role:</strong> {user.userRole}</p>
+            </div>
 
-      {user.userRole === 'MANAGER' || user.userRole === 'ADMIN' ? (
-        <div className="mt-4">
-          <h2 className="text-xl font-bold mb-2">Admin Actions</h2>
-          <p>Here you can add admin-specific actions and details.</p>
-        </div>
-      ) : (
-        <div className="mt-4">
-          <h2 className="text-xl font-bold mb-2">User Actions</h2>
-          <p>Here you can add user-specific actions and details.</p>
-        </div>
-      )}
+            {user.userRole === 'MANAGER' || user.userRole === 'ADMIN' ? (
+              <div className="mt-4">
+                <h2 className="text-xl font-bold mb-2">Admin Actions</h2>
+                <p>Here you can add admin-specific actions and details.</p>
+              </div>
+            ) : (
+              <div className="mt-4">
+                <h2 className="text-xl font-bold mb-2">User Actions</h2>
+                <p>Here you can add user-specific actions and details.</p>
+              </div>
+            )}
+          </Tab.Panel>
+          <Tab.Panel>
+            <div className="space-y-3">
+              <AddSubject userEmail={session.user.email} />
+              <ChangePassword sessionData={session.user.email} />
+            </div>
+          </Tab.Panel>
+          <Tab.Panel>
+            <ShowData userID={session.user.sub} />
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 };
