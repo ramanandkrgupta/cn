c5ae6fdff509cd0afcf1436063ec2a9cd5e47f44
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-08-29 08:05:35 +0530

    Update page.jsx

--- app/(root)/(home)/page.jsx
+++ app/(root)/(home)/page.jsx
@@ -16,7 +16,7 @@ export default function Home() {
       </div>
 
       {/* Feed Section */}
-      <div className="w-full px-4 md:px-8 ">
+      <div className="w-full md:px-8 ">
         <Feed
           label="Discover Courses"
           styleHead="mt-3"
