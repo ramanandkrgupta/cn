946b11264d9d870c2813e48079feea58ea5a00b4
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-08-29 07:18:05 +0530

    Update BottomMenuBar.jsx

--- components/navigation/BottomMenuBar.jsx
+++ components/navigation/BottomMenuBar.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 import { useRouter } from 'next/navigation';  // Correct import
 import { HomeIcon, ArrowUpOnSquareIcon, UserIcon, ShareIcon, DocumentTextIcon } from '@heroicons/react/24/outline';
