bc745d695fb9957365c45f96fb94c7ef42bbcc05
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-08-29 13:15:10 +0530

    Update index.js

--- components/navigation/index.js
+++ components/navigation/index.js
@@ -2,4 +2,5 @@ export { default as Navbar } from "./Navbar";
 export { default as Sidebar } from "./Sidebar";
 
 export {default as StickyBottomMenu } from "./StickyBottomMenu";
+export {default as NewNavBar } from "./NewNavBar";
 
