fba4dbfa5c5458c097c2978b97974c41e4d854eb
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-08-29 18:18:43 +0530

    Update NewNavbar.jsx

--- components/navigation/NewNavbar.jsx
+++ components/navigation/NewNavbar.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react';
-import { IonIcon } from 'react-ionicons';
+import { IonIcon } from "@ionic/react";
 import {
   homeOutline,
   folderOutline,
