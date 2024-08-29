022cdced6db88543f969bc02a713cbeb129a0a1d
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-08-30 01:22:43 +0530

    Update route.js

--- app/api/user/account/route.js
+++ app/api/user/account/route.js
@@ -1,37 +1,34 @@
-import { getServerSession } from 'next-auth';
+import { getSession } from 'next-auth/react';
 import prisma from '@/libs/prisma';
 
 export default async function handler(req, res) {
-  const session = await getServerSession({ req });
+  const session = await getSession({ req });
 
   if (!session) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
 
   if (req.method === 'GET') {
-    try {
-      const user = await prisma.user.findUnique({
-        where: {
-          email: session.user.email,
-        },
-        select: {
-          email: true,
-          name: true,
-          phoneNumber: true,
-          userRole: true,
-        },
-      });
+    // Handle GET request
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session.user.email,
+      },
+      select: {
+        email: true,
+        name: true,
+        phoneNumber: true,
+        userRole: true,
+      },
+    });
 
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-
-      return res.status(200).json(user);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: 'Internal Server Error' });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
     }
+
+    return res.status(200).json(user);
   } else if (req.method === 'POST') {
+    // Handle POST request
     const { name, phoneNumber } = req.body;
 
     try {
