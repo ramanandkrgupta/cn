ad0b3c5cc6e1a0661a26e9965060e8afde05328c
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-08-31 12:34:02 +0530

    Update route.js

--- app/api/user/create-order/route.js
+++ app/api/user/create-order/route.js
@@ -5,7 +5,6 @@ export async function POST(req) {
   const body = await req.text();
   const headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Host': 'khilaadixpro.shop'
   };
 
   try {
