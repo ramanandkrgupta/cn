78c518c9998f948337fc25214489a559a56bb68e
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-08-31 05:58:08 +0530

    Update Navbar.jsx

--- components/navigation/Navbar.jsx
+++ components/navigation/Navbar.jsx
@@ -1,4 +1,6 @@
 // File: components/navigation/Navbar.jsx
+
+"use client"
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useMemo, useRef, useState } from "react";
