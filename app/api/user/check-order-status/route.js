ad394e575d2c7871e2be2cdd3b2811c58767fdb8
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-09-02 10:22:18 +0530

    Update route.js

--- app/api/user/check-order-status/route.js
+++ app/api/user/check-order-status/route.js
@@ -1,39 +1,20 @@
 import { NextResponse } from 'next/server';
-import axios from 'axios';
-import qs from 'qs';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export const POST = async (req) => {
   try {
-    const { user_token, order_id, phoneNumber } = await req.json();
+    const { status, order_id, customer_mobile, amount, remark1, remark2 } = await req.json();
 
-    if (!user_token || !order_id || !phoneNumber) {
+    if (!status || !order_id || !customer_mobile) {
       return NextResponse.json({ error: 'Missing required parameters.' }, { status: 400 });
     }
 
-    const data = qs.stringify({
-      user_token,
-      order_id,
-phoneNumber
-    });
-
-    const config = {
-      method: 'post',
-      url: 'https://instantdum.com/api/check-order-status',
-      headers: { 
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      data: data
-    };
-
-    const response = await axios.request(config);
-
-    if (response.data.status && response.data.result.txnStatus === 'SUCCESS') {
-      // Update user role to PRO in the database using Prisma and phoneNumber
+    if (status === 'SUCCESS') {
+      // Update user role to PRO in the database using Prisma and customer_mobile
       await prisma.user.update({
-        where: { phoneNumber: phoneNumber },
+        where: { phoneNumber: customer_mobile },
         data: { userRole: 'PRO' }
       });
 
@@ -43,7 +24,19 @@ phoneNumber
     }
 
   } catch (error) {
-    console.error('Error checking order status:', error);
-    return NextResponse.json({ error: 'An error occurred while checking the order status.' }, { status: 500 });
+    console.error('Error processing webhook:', error);
+
+    // Add more detailed logging
+    if (error.response) {
+      console.error('Error response data:', error.response.data);
+      console.error('Error response status:', error.response.status);
+      console.error('Error response headers:', error.response.headers);
+    } else if (error.request) {
+      console.error('Error request:', error.request);
+    } else {
+      console.error('Error message:', error.message);
+    }
+
+    return NextResponse.json({ error: 'An error occurred while processing the webhook.' }, { status: 500 });
   }
 };
