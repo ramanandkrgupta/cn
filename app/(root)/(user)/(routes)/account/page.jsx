a6451c30a8f41b15fbd60e4c357f81e6ea0f3039
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-09-04 00:46:52 +0530

    Update page.jsx trying

--- app/(root)/(user)/(routes)/account/page.jsx
+++ app/(root)/(user)/(routes)/account/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { toast } from "sonner";
-import { Tab } from "@headlessui/react";
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import { CheckBadgeIcon } from '@heroicons/react/24/outline';
@@ -12,8 +11,6 @@ import { useState } from 'react';
 import Radio, { RadioGroup } from '@/components/Radio.jsx';
 import Plan from '@/components/Plan.jsx';
 import { BadgePercent, Sparkle, Gem, Crown, ArrowRight } from 'lucide-react';
-
-const Tabs = ["Account Details", "Settings"];
 
 const AccountPage = () => {
   const router = useRouter();
@@ -87,105 +84,95 @@ const AccountPage = () => {
     <div className="min-h-screen">
       <div className="container mx-auto p-1 flex gap-6">
         <div className="flex-1 p-1 rounded-lg shadow-md">
-          <Tab.Group>
-            <Tab.List className="flex space-x-1 border-b-2">
-              {Tabs.map((tab, index) => (
-                <Tab
-                  key={index}
-                  className={({ selected }) =>
-                    `py-2 px-4 text-lg font-semibold ${
-                      selected ? "border-b-4 border-green-500" : "text-gray-500"
-                    }`
-                  }
-                >
-                  {tab}
-                </Tab>
-              ))}
-            </Tab.List>
-            <ProfileCard />
-            <Tab.Panels className="mt-6">
-              <Tab.Panel>
-                <div className="bg-gray-50 p-6 rounded-lg shadow-sm">
-                  <h2 className="text-2xl font-bold mb-4">Account Details</h2>
-                  <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-                    <div>
-                      <p className="text-gray-700 flex">
-                        <strong>Name:</strong> {user.name} 
-                        <CheckBadgeIcon className="h-5 w-5 text-green-500 ml-2" />
-                      </p>
-                      <p className="text-gray-700"><strong>Email:</strong> {user.email}</p>
-                      <p className="text-gray-700"><strong>Phone Number:</strong> {user.phoneNumber || 'N/A'}</p>
-                      <p className="text-gray-700"><strong>Plan:</strong> {user.userRole || 'N/A'}</p>
-                    </div>
-                  </div>
+          <ProfileCard />
+          <div className="mt-6 bg-gray-50 p-6 rounded-lg shadow-sm">
+            <h2 className="text-2xl font-bold mb-4">Account Details</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+              <div>
+                <div className="text-gray-700 flex mb-2">
+                  <strong>Name:</strong>
+                  <span className="ml-2">{user.name}</span>
+                  <CheckBadgeIcon className="h-5 w-5 text-green-500 ml-2" />
                 </div>
+                <div className="text-gray-700 flex mb-2">
+                  <strong>Email:</strong>
+                  <span className="ml-2">{user.email}</span>
+                </div>
+                <div className="text-gray-700 flex mb-2">
+                  <strong>Phone Number:</strong>
+                  <span className="ml-2">{user.phoneNumber || 'N/A'}</span>
+                </div>
+                <div className="text-gray-700 flex mb-2">
+                  <strong>Plan:</strong>
+                  <span className="ml-2">{user.userRole || 'N/A'}</span>
+                </div>
+              </div>
+            </div>
+          </div>
 
-                {user.userRole === 'MANAGER' || user.userRole === 'ADMIN' ? (
-                  <div className="mt-8 bg-gray-50 p-6 rounded-lg shadow-sm">
-                    <h2 className="text-2xl font-bold mb-4">Admin Actions</h2>
-                    <p className="text-gray-700">Here you can add admin-specific actions and details.</p>
-                  </div>
-                ) : (
-                  <div className="mt-8 bg-gray-50 p-6 rounded-lg shadow-sm">
-                    <h2 className="text-2xl font-bold mb-4">User Actions</h2>
-                    <p className="text-gray-700">Here you can add user-specific actions and details.</p>
-                  </div>
-                )}
+          {user.userRole === 'MANAGER' || user.userRole === 'ADMIN' ? (
+            <div className="mt-8 bg-gray-50 p-6 rounded-lg shadow-sm">
+              <h2 className="text-2xl font-bold mb-4">Admin Actions</h2>
+              <p className="text-gray-700">Here you can add admin-specific actions and details.</p>
+            </div>
+          ) : (
+            <div className="mt-8 bg-gray-50 p-6 rounded-lg shadow-sm">
+              <h2 className="text-2xl font-bold mb-4">User Actions</h2>
+              <p className="text-gray-700">Here you can add user-specific actions and details.</p>
+            </div>
+          )}
 
-                <div className="mt-8 bg-gray-50 p-6 rounded-lg shadow-sm">
-                  <h2 className="text-2xl font-bold mb-4">Choose Your Plan</h2>
-                  <RadioGroup value={plan} onChange={(e) => setPlan(e.target.value)}>
-                    <div className="flex gap-4 flex-col">
-                      <Radio value="free">
-                        <Plan
-                          icon={<BadgePercent />}
-                          title="Free"
-                          features={["SD (480p)", "Mobile", "Ads"]}
-                          price={0}
-                        />
-                      </Radio>
-                      <Radio value="basic">
-                        <Plan
-                          icon={<Sparkle />}
-                          title="Basic"
-                          features={["HD (720p)", "1 Device"]}
-                          price={4.99}
-                        />
-                      </Radio>
-                      <Radio value="standard">
-                        <Plan
-                          icon={<Gem />}
-                          title="Standard"
-                          features={["Full HD (1080p)", "2 Devices"]}
-                          price={9.99}
-                        />
-                      </Radio>
-                      <Radio value="premium">
-                        <Plan
-                          icon={<Crown />}
-                          title="Premium"
-                          features={["Ultra HD (4K) + HDR", "4 Devices"]}
-                          price={14.99}
-                        />
-                      </Radio>
-                    </div>
-                  </RadioGroup>
-                  <button
-                    className="mt-4 flex gap-4 items-center px-6 py-3 rounded-lg bg-violet-800 hover:bg-violet-700 font-semibold text-lg text-white"
-                    onClick={handleSubscribe}
-                  >
-                    Proceed with {plan} plan
-                    <ArrowRight />
-                  </button>
-                </div>
-              </Tab.Panel>
-              <Tab.Panel>
-                <div className="bg-gray-50 p-6 rounded-lg shadow-sm space-y-6">
-                  <ChangePassword sessionData={user.email} />
-                </div>
-              </Tab.Panel>
-            </Tab.Panels>
-          </Tab.Group>
+          <div className="mt-8 bg-gray-50 p-6 rounded-lg shadow-sm">
+            <h2 className="text-2xl font-bold mb-4">Choose Your Plan</h2>
+            <RadioGroup value={plan} onChange={(e) => setPlan(e.target.value)}>
+              <div className="flex gap-4 flex-col">
+                <Radio value="free">
+                  <Plan
+                    icon={<BadgePercent />}
+                    title="Free"
+                    features={["SD (480p)", "Mobile", "Ads"]}
+                    price={0}
+                  />
+                </Radio>
+                <Radio value="basic">
+                  <Plan
+                    icon={<Sparkle />}
+                    title="Basic"
+                    features={["HD (720p)", "1 Device"]}
+                    price={4.99}
+                  />
+                </Radio>
+                <Radio value="standard">
+                  <Plan
+                    icon={<Gem />}
+                    title="Standard"
+                    features={["Full HD (1080p)", "2 Devices"]}
+                    price={9.99}
+                  />
+                </Radio>
+                <Radio value="premium">
+                  <Plan
+                    icon={<Crown />}
+                    title="Premium"
+                    features={["Ultra HD (4K) + HDR", "4 Devices"]}
+                    price={14.99}
+                  />
+                </Radio>
+              </div>
+            </RadioGroup>
+            <button
+              className="mt-4 flex gap-4 items-center px-6 py-3 rounded-lg bg-violet-800 hover:bg-violet-700 font-semibold text-lg text-white"
+              onClick={handleSubscribe}
+            >
+              Proceed with {plan} plan
+              <ArrowRight />
+            </button>
+          </div>
+
+          <div className="mt-8 bg-gray-50 p-6 rounded-lg shadow-sm">
+            <h2 className="text-2xl font-bold mb-4">Change Password</h2>
+            <ChangePassword sessionData={user.email} />
+          </div>
         </div>
       </div>
     </div>
