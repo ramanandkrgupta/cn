ec438423e0250a21ebccd407deeb71a5ab6ec0bc
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-09-03 16:45:50 +0000

    responsive

--- app/(root)/(user)/(routes)/account/ProfileCard.jsx
+++ app/(root)/(user)/(routes)/account/ProfileCard.jsx
@@ -2,29 +2,29 @@ import React from 'react';
 
 const ProfileCard = () => {
   return (
-    <div className="bg-gray-900 p-8 rounded-lg shadow-lg">
-      <div className="flex justify-between items-center mb-6">
-        <div className="flex items-center">
+    <div className="bg-gray-900 p-6 md:p-8 rounded-lg shadow-lg">
+      <div className="flex flex-col md:flex-row justify-between items-center mb-6">
+        <div className="flex items-center mb-4 md:mb-0">
           <img
-            src="https://i.imgur.com/J43F22c.png"
+            src="img/profile.jpg"
             alt="Profile Picture"
             className="w-16 h-16 rounded-full mr-4"
           />
           <div>
-            <h2 className="text-2xl font-bold text-white">Neelesh Chaudhary</h2>
+            <h2 className="text-xl md:text-2xl font-bold text-white">Neelesh Chaudhary</h2>
             <p className="text-gray-400">UI / UX Designer</p>
           </div>
         </div>
-        <div className="flex space-x-4">
-          <div>
+        <div className="flex space-x-6 md:space-x-4">
+          <div className="text-center">
             <p className="text-white font-medium">Post</p>
             <p className="text-lg font-bold text-white">23</p>
           </div>
-          <div>
+          <div className="text-center">
             <p className="text-white font-medium">Followers</p>
             <p className="text-lg font-bold text-white">326</p>
           </div>
-          <div>
+          <div className="text-center">
             <p className="text-white font-medium">Following</p>
             <p className="text-lg font-bold text-white">120</p>
           </div>
@@ -32,7 +32,7 @@ const ProfileCard = () => {
       </div>
       <div>
         <p className="text-white font-medium mb-2">About</p>
-        <p className="text-gray-400">
+        <p className="text-gray-400 text-sm md:text-base">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare
           pretium placerat ut platea. Purus blandit integer sagittis massa vel
           est hac.
