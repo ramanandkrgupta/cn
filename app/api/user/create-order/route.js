c75f02fdeb5c33ebe41daf82773591d8c37a4fc0
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-10-26 12:49:34 +0530

    Update route.js

--- app/api/user/create-order/route.js
+++ app/api/user/create-order/route.js
@@ -4,7 +4,7 @@ import qs from 'qs';
 
 export const POST = async (req) => {
   try {
-    const user_token = '271efbf1b89e030bfbc30fb05ebd6af9';
+    const user_token = 'bd53cb790ccc318001ab4dd05f68e767';
     const { customer_mobile, amount, order_id, redirect_url, remark1, remark2, route } = await req.json();
 
     if (!customer_mobile || !amount || !order_id || !redirect_url || !remark1 || !remark2 || !route) {
@@ -25,10 +25,10 @@ export const POST = async (req) => {
     const config = {
       method: 'post',
       maxBodyLength: Infinity,
-      url: 'https://pay.collegenotes.tech/api/create-order',
+      url: 'https://beta.collegenotes.tech/api/create-order',
       headers: { 
-        'Host': 'pay.collegenotes.tech', 
-        'Origin': 'https://pay.collegenotes.tech', 
+        'Host': 'beta.collegenotes.tech', 
+        'Origin': 'https://beta.collegenotes.tech', 
         'Access-Control-Allow-Origin': '*', 
         'Content-Type': 'application/x-www-form-urlencoded'
       },
