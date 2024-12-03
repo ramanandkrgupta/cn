115174e93f1a8542386548b15decbd4220429892
ramanandkrgupta <rgupta1842005@gmail.com>
2024-12-03 15:06:23 +0530

    nn

--- app/(root)/(user)/(routes)/downloads/page.jsx
+++ app/(root)/(user)/(routes)/downloads/page.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Download } from "lucide-react";
 import { useRouter } from "next/navigation";
 import PostCard from "@/components/cards/PostCard";
 
