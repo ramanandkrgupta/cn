b201c31ebae32ce9bdae950d7a08d27608539963
ramanandkrgupta <rgupta1842005@gmail.com>
2025-01-16 00:45:51 +0530

    add tracking

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -3,11 +3,9 @@
 import { Toaster } from "react-hot-toast";
 import { Navigation } from "@/components/navigation";
 
-
 export default function RootLayout({ children }) {
   return (
     <>
- 
       <div className="min-h-screen bg-base-100">
         <Toaster position="top-center" />
         <main className="pb-16 sm:pb-0">{children}</main>
@@ -16,10 +14,18 @@ export default function RootLayout({ children }) {
       <script
         defer
         src="https://cloud.umami.is/script.js"
-        data-website-id=""
+        data-website-id="ce416d58-503a-40cd-9663-7e7618ecc8f7"
       ></script>
-     
-      
- </>
+
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-DFFQPQG8G4"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-DFFQPQG8G4');
+      </script>
+    </>
   );
 }
