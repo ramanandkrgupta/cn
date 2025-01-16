5d5b8bf10dcccad0129a85c2ea7e602b82e6b4a8
Nitesh Chourasiya <140261740+jattu8602@users.noreply.github.com>
2025-01-16 21:05:50 +0530

    added iot,aids courses there

--- public/course/icons/index.js
+++ public/course/icons/index.js
@@ -5,8 +5,10 @@ import ce from "./ce.webp";
 import ec from "./ec.webp";
 import ex from "./ex.webp";
 import me from "./me.webp";
+import aids from "./aids.png";
+import iot from "./iot.png";
 
 
-export { aiml, bca, btech, ce, ec, ex, me };
+export { aiml, bca, btech, ce, ec, ex, me,iot,aids};
 
 
