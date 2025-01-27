67ff42aada46db6fc7ddeba85481e623e8348e4a
Nitesh Chourasiya <140261740+jattu8602@users.noreply.github.com>
2025-01-27 12:44:17 +0530

    improved loading state

---
+++ app/(root)/(user)/(routes)/account/profile/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => (
+  <div className="min-h-screen flex items-center justify-center">
+    <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-primary"></div>
+  </div>
+)
+
+export default Loading
